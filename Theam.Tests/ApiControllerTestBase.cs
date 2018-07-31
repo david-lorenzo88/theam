40f8e3755bff8a338ee48a6eea22f811a61334aa
David <dlorenzo.1988@gmail.com>
2018-07-31 19:57:32 +0100

    Added Seeder call to Test Project

--- Theam.Tests/ApiControllerTestBase.cs
+++ Theam.Tests/ApiControllerTestBase.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using Theam.API.Data;
 
 namespace Theam.Tests
 {
@@ -16,8 +18,20 @@ namespace Theam.Tests
         {
             var builder = WebHost.CreateDefaultBuilder()
                 .UseStartup<Startup>();
-            
+
             var server = new TestServer(builder);
+
+            //Launch DB Seeder
+            using (var scope = server.Host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                using (var context = services.GetRequiredService<ApiContext>())
+                {
+                    DbSeeder.Initialize(context).Wait();
+                }
+            }
+
             var client = server.CreateClient();
             
             client.DefaultRequestHeaders.Clear();
