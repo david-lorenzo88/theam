cd9cf64f0e77429f991fe0bc0d7cb0b4bbd2ce64
David <dlorenzo.1988@gmail.com>
2018-08-03 14:25:56 +0100

    Added Required attributes to UserDTO

--- Theam/Models/Dto/UserDTO.cs
+++ Theam/Models/Dto/UserDTO.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,12 @@ namespace Theam.API.Models
     public class UserDTO
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Surname { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
         public virtual List<UserRoleDTO> Roles { get; set; }
     }
